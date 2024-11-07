d697a99460ff4b4360536518cdfd4921a4555e08
Zep213 <jososephbem100@gmail.com>
2024-11-07 16:09:16 -0300

    ProjetoPOO

--- src/br/com/catolica/hotel/Main/Main.java
+++ src/br/com/catolica/hotel/Main/Main.java
@@ -1,5 +1,6 @@
 package br.com.catolica.hotel.Main;
 
+import br.com.catolica.hotel.Enums.EnumCargos;
 import br.com.catolica.hotel.Enums.EnumTipoCliente;
 import br.com.catolica.hotel.Enums.EnumTipoQuartos;
 import br.com.catolica.hotel.Models.*;
@@ -18,7 +19,7 @@ public class Main {
         ClienteVip[] clientesVips = new ClienteVip[6];
         QuartoConv[] quartoConvs = new QuartoConv[6];
         QuartoVip[] quartoVips = new QuartoVip[6];
-        Funcionario[] funcionarios = new Funcionario[6];
+        Recepcionista[] recepcionistas = new Recepcionista[6];
         Hotel hotel = new Hotel("Hotel transilvania","633424",quartoConvs,quartoVips);
         
         while (true) {
@@ -35,12 +36,19 @@ public class Main {
                     System.out.println("Usuario ou senha incorretos, tente novamente");
                 }
             }else {
-                for (int i = 1; i < quartoConvs.length; i++) {
+                for (int i = 0; i < quartoConvs.length; i++) {
                     if (quartoConvs[i] == null){
                         quartoConvs[i] = new QuartoConv(i, false,null);
                         System.out.println(quartoConvs[i]);
                     }
                 }
+                for (int i = 0; i < quartoVips.length; i++) {
+                    if (quartoVips[i] == null){
+                        quartoVips[i] = new QuartoVip(i, false,null);
+                        System.out.println(quartoConvs[i]);
+                    }
+                }
+
                 System.out.println("\n--- Menu ADM ---");
                 System.out.println("1. Cadastrar Cliente");
                 System.out.println("2. Cadastrar ClienteVip");
@@ -51,72 +59,98 @@ public class Main {
                 System.out.print("Escolha uma opçao: ");
                 int opcao = input.nextInt();;
 
-                switch (opcao){
+                switch (opcao) {
 
                     case 1:
-                        System.out.print("Quantos Clientes deseja cadastrar? ");
-                        int qntdCli = input.nextInt();
-                        for (int c = 0; c < qntdCli; c++) {
-                            System.out.println("--- Cadastrar Clientes ---");
-                            System.out.print("Digite seu nome: ");
-                            String nome = input.next();
-                            if (nome.length() < 2){
-                                System.out.println("digite um nome valido!");
-                                break;
-                            }
-                            System.out.print("Digite seu documento: ");
-                            String doc = input.next();
-                            if (doc.length() < 3) {
-                                System.out.println("digite um documento valido");
-                                break;
-                            }
-
-                            clientes[c] = new ClienteConv(nome,doc,EnumTipoCliente.CONVENCIONAL);
-                            System.out.print("em qual quarto deseja se hospedar? ");
-                            int numQuarto = input.nextInt();
-                            for (int i = 1; i < quartoConvs.length; i++) {
-                                if (numQuarto > 0 && numQuarto == quartoConvs[i].getNum()){
-                                    quartoConvs[numQuarto] = new QuartoConv(numQuarto,true,new ClienteConv(nome,doc,EnumTipoCliente.CONVENCIONAL));
-                                }
-                            }
-                            System.out.println("Cliente cadastrado com Sucesso");
+                        System.out.println("--- Cadastrar Clientes ---");
+                        System.out.print("Digite seu nome: ");
+                        String nome = input.next();
+                        if (nome.length() < 2) {
+                            System.out.println("digite um nome valido!");
                             break;
                         }
+                        System.out.print("Digite seu documento: ");
+                        String doc = input.next();
+                        if (doc.length() < 3) {
+                            System.out.println("digite um documento valido");
+                            break;
+                        }
+                        for (int i = 0; i < clientes.length; i++) {
+                            if(clientes[i] == null) {
+                                clientes[i] = new ClienteConv(nome, doc, EnumTipoCliente.CONVENCIONAL);
+                                System.out.println("cliente cadastrado com sucesso");
+                                break;
+                            }
+                        }
                         break;
-
                     case 2:
                         System.out.println("--- Cadastrar Clientes Vip's ---");
                         System.out.print("Digite seu nome:");
                         String nomeVip = input.next();
                         System.out.print("Digite seu documento: ");
                         String docVip = input.next();
-                        //sistema de adicionar a lista
+                        for (int i = 0; i < clientes.length; i++) {
+                            if(clientes[i] == null) {
+                                clientes[i] = new ClienteConv(nomeVip, docVip, EnumTipoCliente.VIP);
+                                System.out.println("cliente cadastrado com sucesso");
+                                break;
+                            }
+                        }
+
                         System.out.println("Cliente Vip cadastrado com sucesso!!");
                         break;
                     case 3:
                         System.out.println("--- Clientes Convencionais ---");
-                        for (int i = 1; i < clientes.length; i++) {
+                        for (int i = 0; i < clientes.length; i++) {
                             if (clientes[i] == null) {
-                                System.out.println("Quarto Livre!!");
+                                System.out.println("espaço livre!!");
                             }else {
-                                System.out.println(quartoConvs[i].toString());
+                                System.out.println(clientes[i].getNome());
                             }
                         }
                         System.out.println("--- Clientes Vip's ---");
-                        for (int i = 1; i < clientesVips.length; i++) {
+                        for (int i = 0; i < clientesVips.length; i++) {
                             if (clientesVips[i] == null) {
-                                System.out.println("Quarto Livre!!");
+                                System.out.println("espaço Livre!!");
                             }else {
-                                System.out.println(clientesVips[i].getNome());
+                                System.out.println("quarto ocupado por " + clientesVips[i].getNome());
                             }
                         }
                         break;
                     case 4:
-                        System.out.println("--- Cadastrar Funcionarios ---");
-                        System.out.println("Digite seu nome: ");
-                        String nomeFunc = input.nextLine();
-                        System.out.println("Digite a data da contrataçao: ");
+                        System.out.println("--- Cadastrar Recepcionista ---");
+                        System.out.print("Digite seu nome: ");
+                        String nomefunc = input.nextLine();
+                        System.out.print("Salario: ");
+                        int salario = input.nextInt();
+                        System.out.print("Dia: ");
+                        int dia = input.nextInt();
+                        System.out.print("Mes: ");
+                        int mes = input.nextInt();
+                        System.out.print("Ano: ");
+                        int ano = input.nextInt();
+                        Data dataContract = new Data(dia,mes,ano);
+                        for (int i = 0; i < recepcionistas.length; i++) {
+                            if (recepcionistas[i] == null){
+                                recepcionistas[i] = new Recepcionista(nomefunc,EnumCargos.RECEPCIONISTA,dataContract,salario,hotel);
+                                System.out.println("Funcionario contratado com sucesso!!");
+                                break;
+                            }
+
+                        }
+
+
+                    case 5:
+                        System.out.println("--- Mostrar Funcionarios ---");
+                        for (int i = 0; i < recepcionistas.length; i++) {
+                            if (recepcionistas[i] == null) {
+                                System.out.println("espaço Livre!!");
+                            }else {
+                                System.out.println(recepcionistas[i].getNome());
+                            }
+                        }
                         break;
+
                 }
 
             }
--- src/br/com/catolica/hotel/Models/ClienteConv.java
+++ src/br/com/catolica/hotel/Models/ClienteConv.java
@@ -6,13 +6,22 @@ import br.com.catolica.hotel.Enums.EnumTipoCliente;
 public class ClienteConv extends Cliente implements IAcessoCliConv {
     Hotel hotel;
     ClienteConv cliente;
+    QuartoConv[] quartoConv;
     public ClienteConv(String nome, String doc, EnumTipoCliente tipoCliente) {
         super(nome, doc, tipoCliente);
     }
     @Override
     public boolean reservar(Quarto quarto, Cliente cli, int numQuarto) {
+        hotel.getQuartos(numQuarto);
+        if (quarto != null && !quarto.isOcupado()) {
+            quarto.setOcupado(cliente);
+            System.out.printf("O quarto %d foi ocupado por %s", numQuarto, cliente.getNome());
+        } else {
+            System.out.printf("O quarto %d ja esta ocupado ou é invalido", numQuarto);
+        }
         return false;
     }
+
 
     @Override
     public boolean checkOut(Quarto quarto, Cliente cli, int numQuarto) {
@@ -22,5 +31,4 @@ public class ClienteConv extends Cliente implements IAcessoCliConv {
         }
         return false;
     }
-
 }
